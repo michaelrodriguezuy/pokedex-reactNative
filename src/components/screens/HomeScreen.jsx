4951817481559302d5d466de90505bcc8d9af468
olimarteam <73653954+olimarteam@users.noreply.github.com>
2024-01-21 22:22:34 -0300

    primera version de mi primera App

---
+++ src/components/screens/HomeScreen.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function HomeScreen() {
+  return (
+    <View>
+      <Text>Home de Michael</Text>
+    </View>
+  );
+}
